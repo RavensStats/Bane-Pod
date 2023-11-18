4db0a35b1b5f5b509cca41c927d5d0f7f388e98a
RavensStats <68250748+RavensStats@users.noreply.github.com>
2023-11-18 17:23:19 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "829926179552235520"; // your league ID
+export const leagueID = "920467985213583360"; // your league ID
 export const leagueName = "Marvel VS DC - Bane Pod"; // your league name
 export const dues = 25; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
